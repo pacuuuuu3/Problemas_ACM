d81a9b04689dc9393e8522f373677a5798d1b63c
Victor <victorzamoragutierrez@gmail.com>
2015-02-28 02:18:08 -0600

    Primer problema iniciado

---
+++ 10142/Australian.c
@@ -0,0 +1,11 @@
+/* Resuelve el problema "Australian Voting" (UVA ID = 10142), utilizando 
+ * arreglos */
+int main () 
+{
+  int casos; /* El numero de casos */
+  scanf("%d", &casos);
+  for(int i = 0; i < casos; ++i){
+    int candidatos; /* El numero de candidatos */
+  }
+  return 0;
+}
